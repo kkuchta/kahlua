4c620a4b65bdef43d951fb4bce5f18173ab402a4
Kevin Kuchta <kevinmkuchta@gmail.com>
2012-11-25 19:39:43 -0800

    Removed further cruft

--- app.rb
+++ app.rb
@@ -3,19 +3,6 @@ require './lib/utils'
 set :environment, :development
 require "sinatra/reloader" if development?
 
-get '/hi' do
-  "Hello World!"
-end
-
-get '/id/?:id?' do
-  id = params[:id] ? params[:id] : randomID
-  if(!validID?(id))
-    "invalid id"
-  else
-    id
-  end
-end
-
 get '/' do
   haml :index
 end
