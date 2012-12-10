346723f56d478873fb2c82a2c73a8b2fd58e038b
Kevin Kuchta <kevinmkuchta@gmail.com>
2012-12-10 00:00:31 -0800

    Starting in on objectifying this thing

--- public/script.js
+++ public/script.js
@@ -21,29 +21,20 @@ $( function(){
 
 function addImages( countToLoad ){
     var countLoaded = 0;
-    var displayImage = function($img){
+    var container = $('#images');
 
-        $img.css('float','left');
-        $('#images').append($img);
-
-        if( controls.kittens.getChecked() ){
-            var width = $img.prop('width');
-            var height = $img.prop('height');
-            var kittenSize = getKittenSize({width:width,height:height});
-            var kittenURL = 'http://placekitten.com/' + kittenSize.width + '/' + kittenSize.height;
-            $img.attr('src',kittenURL);
-        }
-        setImageSize( $img );
-        $('#images').masonry( 'appended', $img, true );
-
-        countLoaded++;
-        if( countLoaded >= countToLoad ){
-            console.log('finished!');
-            $.waypoints('refresh');
-        }
-    };
     for( var i = 0; i < countToLoad; i++ ){
-        getValidImage( displayImage );
+        ImgurImage.createRandom( container, function(imgurImage){
+            //displayImage(imgurImage);
+            imgurImage.display();
+            $('#images').masonry( 'appended', imgurImage.$img, true );
+
+            countLoaded++;
+            if( countLoaded >= countToLoad ){
+                console.log('finished!');
+                $.waypoints('refresh');
+            }
+        });
     }
 }
 
@@ -83,12 +74,6 @@ function hoverHandler( e ){
     }
 }
 
-function setImageSize( $img ){
-    var width = $img.prop('width');
-    var newWidth = fixWidth( width, COLUMN_WIDTH, 3 );
-    $img.css('width',newWidth);
-    var height = $img.css('max-height',1000);
-}
 
 // Expects {width:123,height:123}
 function getKittenSize( rawSize ){
@@ -101,6 +86,14 @@ function getKittenSize( rawSize ){
     return {height:height,width:width};
 }
 
+
+function setImageSize($img){
+    var width = $img.prop('width');
+    var newWidth = fixWidth( width, COLUMN_WIDTH, 3 );
+    $img.css('width',newWidth);
+    var height = $img.css('max-height',1000);
+}
+
 /**
  * Snap width a grid line width (always rounding down)
  */
@@ -111,55 +104,6 @@ function fixWidth( width, gridWidth, maxGridLines ){
         }
     }
     return maxGridLines * gridWidth;
-}
-
-// Gets a valid jquery image object
-function getValidImage( callback ){
-    var url = 'http://i.imgur.com/' + makeid() + '.jpg';
-    var $img = $("<img class='imgurImage' src='" + url + "'>");
-
-    // Hide the image but still add it to the body so the height/width load
-    $img.css('position','absolute');
-    $img.css('left','-10000px');
-    $img.css('top','-10000px');
-    $img.css('opacity','-10000px');
-    $('body').append($img);
-    $img.load(function(){
-        if(
-            (this.width == 161 && this.height == 81) || // kill non-existant images
-            (this.width < COLUMN_WIDTH ) || // kill images smaller than 1 column
-            (this.width < 25 || this.height < 25 ) // kill super thin/tall images
-        ){
-            console.log('gotbadimage');
-            $img.remove();
-            getValidImage(callback);
-        }
-        else{
-            console.log("success");
-            $img.css('position','relative');
-            $img.css('left','0');
-            $img.css('top','0');
-            $img.detach();
-            callback($img);
-        }
-    });
-}
-
-/**
- * Generates a random 5 character alphanumeric string (upper and lower case)
- * From http://stackoverflow.com/questions/1349404/generate-a-string-of-5-random-characters-in-javascript
- */
-function makeid()
-{
-    var text = "";
-    var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-
-    for( var i=0; i < 5; i++ ){
-        text += possible.charAt(Math.floor(Math.random() * possible.length));
-    }
-
-    //console.log( 'id=' + text );
-    return text;
 }
 
 var controls = (function(){
